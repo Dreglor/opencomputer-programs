addb0eca042dee2d10752a4015b195b6e9f1346d
Dreglor <dreglor@gmail.com>
2019-05-31 00:58:39 -0700

    bcopy now works

--- droneutils/package/bin/bcopy.lua
+++ droneutils/package/bin/bcopy.lua
@@ -1,5 +1,5 @@
 local shell = require("shell")
-local filesystem = require("filesystem")
+local io = require("io")
 local serialization = require("serialization")
 
 local log = require("liblog")
@@ -9,9 +9,6 @@ local args, ops = shell.parse(...)
 local usage = "Usage: bcopy FILE DEST\n"
 local source
 local destination
-
-filesystem.
-
 
 if (#ops > 0) then
     print(usage)
@@ -40,11 +37,18 @@ end
 source = args[1]
 destination = args[2]
 
-local handle = filesystem.open(source, 'rb')
+local handle = io.open(source, 'r')
+
+if (handle == nil) then
+    log.fatal("Source File does not exist or could not be opened for reading!")
+end
+
 local data = handle:read("*a")
+handle:close()
 
-local head = "local filesystem=require('filesystem');serialization=require('serialization');local handle=filesystem.open('" .. destination .. "','w');local data=serialization.unserialize('"
-local body = serialization.serialize()
-local tail = "');handle:write(data);handle:close()"
+local head = "local io=require('io');local handle=io.open('" .. destination .. "','w');local data="
+local body = serialization.serialize(data)
+local tail = ";print(data);handle:write(data);handle:close()"
 
+--local temp, _ = string.gsub(head..body..tail,"\\","\\\\")
 drone.Broadcast(head..body..tail)
