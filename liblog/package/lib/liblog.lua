d3a8b40f6bfc99ff96d4f39d0aca177805ac9716
Dreglor <dreglor@gmail.com>
2019-06-08 01:05:51 -0700

    expanded liblog and added libnet

--- liblog/package/lib/liblog.lua
+++ liblog/package/lib/liblog.lua
@@ -1,21 +1,130 @@
 --Logging facility
 
 local lib = {}
+local net = require("libnet")
+local os = require("os")
+
+lib.Level {
+    ["FATAL"] = 1,
+    ["ERROR"] = 2,
+    ["WARNING"] = 3,
+    ["INFO"] = 4,
+    ["DEBUG"] = 5,
+    [5] = "DEBUG",
+    [4] = "INFO",
+    [3] = "WARNING",
+    [2] = "ERROR",
+    [1] = "FATAL",
+}
+
+local DisplayLevel = lib.Level["ERROR"]
+local RecordLevel = lib.Level["WARNING"]
+local BroadcastLevel = lib.Level["INFO"]
+
+local LogName = "general"
+local RecordPath = "/var/log/"
+local LOGPORT = 1110
+
+function lib.SetDisplayLevel(level)
+    if (type(level) ~= "number") then
+        level = lib.Level[level];
+    end
+
+    DisplayLevel = level
+end
+
+function lib.SetRecordLevel(level)
+    if (type(level) ~= "number") then
+        level = lib.Level[level];
+    end
+
+    RecordLevel = level
+end
+
+function lib.SetBroadcastLevel(level)
+    if (type(level) ~= "number") then
+        level = lib.Level[level];
+    end
+
+    BroadcastLevel = level
+end
+
+function lib.SetLogName(name)
+    LogName = name
+end
+
+local function Record(level, message)
+    local numberLevel
+    if (type(level) == "number") then
+        numberLevel = level
+        level = lib.Level[level]
+    else
+        numberLevel = lib.Level[level]
+    end
+
+    if (numberLevel >= RecordLevel) then
+        return
+    end
+
+    local handle = os.open(RecordPath..LogName, "a")
+    handle:write("[" .. tostring(os.time) .. "] <".. level .."> - " .. message)
+    handle:close()
+end
+
+local function Broadcast(level, message)
+    local numberLevel
+    if (type(level) == "number") then
+        numberLevel = level
+        level = lib.Level[level]
+    else
+        numberLevel = lib.Level[level]
+    end
+
+    if (numberLevel >= BroadcastLevel) then
+        return
+    end
+
+    local payload = {Time = os.time(), Name = LogName, Level = level, Message = message}
+    net.Broadcast(payload, LOGPORT)
+end
+
+local function Display(level, message)
+    local numberLevel
+    if (type(level) == "number") then
+        numberLevel = level
+        level = lib.Level[level]
+    else
+        numberLevel = lib.Level[level]
+    end
+
+    if (numberLevel >= DisplayLevel) then
+        return
+    end
+
+    print("[".. level .."] - " .. message)
+end
+
+local function Log(level, message)
+    Display(level, message)
+    Broadcast(level, message)
+    Record(level, message)
+end
 
 function lib.Fatal(message)
-    error("FATAL: " .. message)
+    Log("FATAL", message)
+    error(message)
 end
 
 function lib.Error(message)
-    print("ERROR: " .. message)
+    Log("ERROR", message)
 end
 
 function lib.Warning(message)
-    print("WARNING: " .. message)
+    Log("WARNING", message)
 end
 
 function lib.Info(message)
-    print("INFO: " .. message)
+    Log("INFO", message)
 end
 
 return lib
---
+++ libnet/package/lib/libnet.lua
@@ -0,0 +1,179 @@
+local lib = {}
+
+--includes
+local component = require("component")
+local event = require("event")
+local os = require("os")
+local math = require("math")
+local log = require("liblog")
+
+--const
+local MODEMTYPE = "modem"
+local COMPONENTADDEVENT = "component_added"
+local COMPONENTREMOVEEVENT = "component_removed"
+local MODEMMESSAGEEVENT = "modem_message"
+local WAKEMESSAGE = "WAKE"
+
+--static
+local Interfaces = {} --proxy table for all modem devices
+local PACKETSIZE = 4096 --max packet size default in config is 8192
+
+local MAXPACKETSRATE = 4
+local Timer
+
+--device event handlers
+function lib.OnAdd(_, address, type)
+    if (type ~= MODEMTYPE) then
+        return
+    end
+
+    local modem = component.proxy(address)
+    
+    modem.setWakeMessage(WAKEMESSAGE)
+    Interfaces[address] = {
+        proxy = modem,
+        packetRate = 0
+    }
+end
+
+function lib.OnRemove(_, address, type)
+    if (type ~= MODEMTYPE) then
+        return
+    end
+
+    if (Interfaces[address] == nil) then
+        return
+    end
+
+    --no need to close the port the device removal will do that for us
+    Interfaces[address] = nil
+end
+
+function lib.Cooldown(interface)
+    local device = Interfaces[interface]
+    if (device.packetRate > 0) then
+        device.packetRate = device.packetRate - 1
+    end
+end
+
+local function GatedSend(interface, to, port, payload)
+    local device = Interfaces[interface]
+
+    while (device.packetRate >= MAXPACKETSRATE) do
+        os.sleep(1/MAXPACKETSRATE)
+    end
+    device.PacketRate = device.PacketRate + 1
+
+    return device.proxy.send(to, port, payload)
+end
+
+local function GatedBroadcast(device, port, payload)
+    local device = Interfaces[interface]
+
+    while (device.packetRate >= 0) do
+        os.sleep(1/MAXPACKETSRATE)
+    end
+    device.PacketRate = MAXPACKETSRATE
+
+    return device.proxy.broadcast(port, payload)
+end
+
+function lib.Send(interface, sendto, payload, port)
+    if (#payload < PACKETSIZE) then
+        if (GatedSend(Interfaces[interface], sendto, port, payload) == false) then
+            log.Fatal("Unable to send message, this should always work unless the device has " ..
+                          "been removed!")
+        end
+        return
+    end
+
+    -- if response is too large to send in a single packet then fragment it
+    if (#payload >= PACKETSIZE) then
+        if (#payload == PACKETSIZE) then
+            -- pad out one character to ensure to avoid corner cases
+            payload = payload .. " "
+        end
+
+        --split and send large results
+        local i = 0
+        local fragment = string.sub(payload, 1, PACKETSIZE)
+        while (fragment ~= "") do
+            if (GatedSend(Interfaces[interface], sendto, port, fragment) == false) then
+                log.Fatal("Unable to send message fragment, this should always work unless the device has " ..
+                          "been removed!")
+            end
+
+            i = i + 1
+            fragment = string.sub(payload, (i * PACKETSIZE) + 1, (i + 1) * PACKETSIZE)
+        end
+    end
+end
+
+function lib.Broadcast(payload, port, interface)
+    local broadcasting = {}
+    if (interface == nil) then
+        for address, _ in pairs(Interfaces) do
+            broadcasting[#broadcasting + 1] = address
+        end
+    else
+        broadcasting = {interface}
+    end
+
+    for _, address in ipairs(broadcasting) do
+        if (#payload < PACKETSIZE) then
+            if (GatedBroadcast(Interfaces[address], port, payload) == false) then
+                log.Fatal("Unable to broadcast message, this should always work unless the device has " ..
+                              "been removed!")
+            end
+        else
+            if (#payload == PACKETSIZE) then
+                -- pad out one character to ensure to avoid corner cases
+                payload = payload .. " "
+            end
+
+            --split and send large results
+            local i = 0
+            local fragment = string.sub(payload, 1, PACKETSIZE)
+            while (fragment ~= "") do
+                if (GatedBroadcast(Interfaces[address], port, fragment) == false) then
+                    log.Fatal("Unable to broadcast message, this should always work unless the device has " ..
+                              "been removed!")
+                end
+
+                i = i + 1
+                fragment = string.sub(payload, (i * PACKETSIZE) + 1, (i + 1) * PACKETSIZE)
+            end
+        end
+    end
+end
+
+--event Handlers
+function lib.OnNetwork(_, sentTo)
+    local device = Interfaces[sentTo]
+    device.packetRate = device.packetRate + 1
+end
+
+function lib.Wake(interface, address)
+    if (interface == nil) then
+        if (address == nil) then
+            for device, _ in pairs(Interfaces) do
+                lib.GatedBroadcast(device, 1, WAKEMESSAGE)
+            end
+        else
+            for device, _ in pairs(Interfaces) do
+                lib.GatedSend(device, address, 1, WAKEMESSAGE)
+            end
+        end
+    else
+        lib.GatedSend(interface, address, 1, WAKEMESSAGE)
+    end
+end
+
+if (Timer == nil) then
+    Timer = event.timer(1/MAXPACKETSRATE, lib.Cooldown, math.huge)
+    event.listen(COMPONENTADDEVENT, lib.OnAdd)
+    event.listen(COMPONENTREMOVEEVENT, lib.OnRemove)
+    event.listen(MODEMMESSAGEEVENT, lib.OnNetwork)
+end
+
+return lib
